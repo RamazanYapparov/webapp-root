ced26d23f79122c572d3688fd0456dcf71d6af09
Waltorw <ramazan20082@yandex.ru>
2016-04-21 18:01:36 +0300

    Encapsulated entity

--- dao/src/main/java/edu/itis/webapp/dao/entities/User.java
+++ dao/src/main/java/edu/itis/webapp/dao/entities/User.java
@@ -13,60 +13,58 @@ public class User implements GenericEntity {
 
     @Id
     @Column(name = "id")
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	public Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	@NotEmpty(message = "заполните поле")
+    @NotEmpty(message = "заполните поле")
     @Column(name = "firstname")
-	public String firstname;
+    private String firstname;
 
-	@NotEmpty(message = "заполните поле")
+    @NotEmpty(message = "заполните поле")
     @Column(name = "lastname")
-	public String lastname;
+    private String lastname;
 
     @NotNull(message = "заполните поле")
-	@Min(value = 18, message = "должен быть совершеннолетним")
+    @Min(value = 18, message = "должен быть совершеннолетним")
     @Column(name = "age")
-	public Integer age;
-	
-	public String getFirstname() {
-		return firstname;
-	}
+    private Integer age;
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public Integer getAge() {
-		return age;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    public Integer getAge() {
+        return age;
+    }
 
-	@Override
-	public Long getId() {
-		return id;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	@Override
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Override
+    public Long getId() {
+        return id;
+    }
 
-	@Override
-    public String toString() {
-		return (this.id + " " + this.firstname + " " + this.lastname + " " + this.age);
-		
-	}
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFio() {
+        return firstname + " " + lastname;
+    }
 
 }
